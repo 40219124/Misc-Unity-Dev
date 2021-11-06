990f0a319aced29294d12fba13bfd7aa42e40105
Timothy Sanderson <t.s.sanderson@hotmail.co.uk>
2021-11-06 00:50:17 +0000

    Planet: Vertices matching, and neater triangles

--- TheNebula/Assets/Planet/PlanetShaper.cs
+++ TheNebula/Assets/Planet/PlanetShaper.cs
@@ -21,6 +21,8 @@ public class PlanetShaper : MonoBehaviour
             meshes[i] = new Mesh();
             meshFilters[i].mesh = meshes[i];
         }
+        facePrefab.gameObject.SetActive(false);
+
         faceRotations[(int)eDirection.right] = Quaternion.FromToRotation(Vector3.back, Vector3.right);
         faceRotations[(int)eDirection.up] = Quaternion.FromToRotation(Vector3.back, Vector3.up);
         faceRotations[(int)eDirection.forward] = Quaternion.Euler(0, 180, 0);
@@ -44,8 +46,6 @@ public class PlanetShaper : MonoBehaviour
         int heightVerts = 15;
         float heightSpacer = height / (heightVerts - 1);
 
-        Vector3 n45 = (Quaternion.Euler(0, 45, 0) * Vector3.back).normalized;
-
         for (int i = 0; i < 6; ++i)
         {
             List<Vector3> verts = new List<Vector3>();
@@ -62,12 +62,14 @@ public class PlanetShaper : MonoBehaviour
                 Vector3 xRot = Quaternion.Euler(xAngle, 0.0f, 0.0f) * Vector3.back;
                 Vector3 newUp = Vector3.Cross(xRot, Vector3.right).normalized;
 
-                Vector3 meeting = Vector3.Cross(newUp, n45).normalized;
-                float alpha = Vector3.Angle(meeting, xRot);
-
                 for (float x = -widthHalf; x <= widthHalf; x += widthSpacer)
                 {
-                    verts.Add(faceQuat * (Quaternion.AngleAxis(alpha * x / widthHalf, newUp) * xRot));
+                    Vector3 vertCircleNorm = (Quaternion.Euler(0, 90 - 45 * Mathf.Abs(x) / widthHalf, 0) * Vector3.back).normalized;
+
+                    Vector3 meeting = Vector3.Cross(newUp, vertCircleNorm).normalized;
+                    float alpha = Vector3.Angle(meeting, xRot);
+
+                    verts.Add(faceQuat * (Quaternion.AngleAxis(alpha * Mathf.Sign(x), newUp) * xRot));
                 }
             }
 
@@ -78,18 +80,38 @@ public class PlanetShaper : MonoBehaviour
                 for (int x = 0; x < widthVerts; ++x)
                 {
                     uvs.Add(new Vector2(x / (widthVerts - 1.0f), y / (heightVerts - 1.0f)));
-                    colours.Add(new Color(uvs[uvs.Count - 1].x, uvs[uvs.Count - 1].y, 0));
-                    //colours.Add(Color.grey);
+
+                    if (false)
+                    {
+                        colours.Add(new Color(uvs[uvs.Count - 1].x, uvs[uvs.Count - 1].y, 0));
+                    }
+                    else
+                    {
+                        colours.Add(Color.grey);
+                    }
 
                     if (x > 0 && y > 0)
                     {
-                        indxs.Add(OneDFromTwoD(x - 1, y - 0, widthVerts));
-                        indxs.Add(OneDFromTwoD(x - 0, y - 1, widthVerts));
-                        indxs.Add(OneDFromTwoD(x - 1, y - 1, widthVerts));
+                        if ((x <= widthVerts / 2 && y <= heightVerts / 2) || (x > widthVerts / 2 && y > heightVerts / 2))
+                        {
+                            indxs.Add(OneDFromTwoD(x - 1, y - 0, widthVerts));
+                            indxs.Add(OneDFromTwoD(x - 0, y - 1, widthVerts));
+                            indxs.Add(OneDFromTwoD(x - 1, y - 1, widthVerts));
 
-                        indxs.Add(OneDFromTwoD(x - 1, y - 0, widthVerts));
-                        indxs.Add(OneDFromTwoD(x - 0, y - 0, widthVerts));
-                        indxs.Add(OneDFromTwoD(x - 0, y - 1, widthVerts));
+                            indxs.Add(OneDFromTwoD(x - 1, y - 0, widthVerts));
+                            indxs.Add(OneDFromTwoD(x - 0, y - 0, widthVerts));
+                            indxs.Add(OneDFromTwoD(x - 0, y - 1, widthVerts));
+                        }
+                        else
+                        {
+                            indxs.Add(OneDFromTwoD(x - 0, y - 0, widthVerts));
+                            indxs.Add(OneDFromTwoD(x - 1, y - 1, widthVerts));
+                            indxs.Add(OneDFromTwoD(x - 1, y - 0, widthVerts));
+
+                            indxs.Add(OneDFromTwoD(x - 0, y - 0, widthVerts));
+                            indxs.Add(OneDFromTwoD(x - 0, y - 1, widthVerts));
+                            indxs.Add(OneDFromTwoD(x - 1, y - 1, widthVerts));
+                        }
                     }
                 }
             }
