4fd1debc3fb7ec4ded86bc59875ee7eaaf0cdb5a
Timothy Sanderson <t.s.sanderson@hotmail.co.uk>
2023-03-18 02:23:31 +0000

    Build in columns!

--- TheNebula/Assets/ForestBuildReplica/BuildingSuggestion.cs
+++ TheNebula/Assets/ForestBuildReplica/BuildingSuggestion.cs
@@ -2,6 +2,77 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public class GridColumn
+{
+    public GridColumn(Vector2Int gridPos, float rawY)
+    {
+        GridPos = gridPos;
+        FloorYRaw = rawY;
+        FloorY = Mathf.FloorToInt(rawY * 4f) / 4f;
+    }
+
+    public Vector2Int GridPos;
+    public float FloorYRaw;
+    public float FloorY;
+    public int BuildStages = 0;
+    public float CurrentY
+    {
+        get
+        {
+            return FloorY + (0.5f * BuildStages);
+        }
+    }
+
+    public Vector3 BasePos
+    {
+        get
+        {
+            return new Vector3(GridPos.x, FloorY, GridPos.y);
+        }
+    }
+    public Vector3 BasePosRaw
+    {
+        get
+        {
+            return new Vector3(GridPos.x, FloorYRaw, GridPos.y);
+        }
+    }
+    public Vector3 TopPos
+    {
+        get
+        {
+            return new Vector3(GridPos.x, CurrentY, GridPos.y);
+        }
+    }
+}
+
+public class BuildingGrid
+{
+    Dictionary<Vector2Int, GridColumn> Columns = new Dictionary<Vector2Int, GridColumn>();
+
+    public Vector2Int ClosestColumnKey(Vector3 pos)
+    {
+        return new Vector2Int(Mathf.RoundToInt(pos.x), Mathf.RoundToInt(pos.z));
+    }
+
+    public GridColumn GetColumn(Vector3 pos)
+    {
+        Vector2Int closest = ClosestColumnKey(pos);
+        if (!Columns.ContainsKey(closest))
+        {
+            Vector3 columnRayStart = new Vector3(closest.x, pos.y, closest.y) + (3f * Vector3.up);
+            Ray forRay = new Ray(columnRayStart, Vector3.down);
+            RaycastHit hit;
+            Physics.Raycast(forRay, out hit, 10f);
+            if (hit.collider.CompareTag("Ground"))
+            {
+                Columns.Add(closest, new GridColumn(closest, hit.point.y));
+            }
+        }
+        return Columns[closest];
+    }
+}
+
 public class BuildingSuggestion : MonoBehaviour
 {
     private static BuildingSuggestion _instance;
@@ -21,6 +92,8 @@ public class BuildingSuggestion : MonoBehaviour
     private float TimeSinceSuggestionMove = 5f;
     private float SuggestionMoveCD = 0.3f;
 
+    BuildingGrid Grid = new BuildingGrid();
+
     public void Initialise(Camera eyes)
     {
         Eyes = eyes;
@@ -37,11 +110,12 @@ public class BuildingSuggestion : MonoBehaviour
     public void PlaceObject(GameObject obj)
     {
         obj.transform.position = DemoObject.position;
+        Grid.GetColumn(obj.transform.position).BuildStages += 4;
     }
 
     void Update()
     {
-        if (Eyes == null )
+        if (Eyes == null)
         {
             return;
         }
@@ -69,8 +143,9 @@ public class BuildingSuggestion : MonoBehaviour
             if (SuggestionFound)
             {
                 Vector3 groundPos = hit.point;
-                Vector3 roundedPos = new Vector3(Mathf.RoundToInt(groundPos.x), Mathf.RoundToInt(groundPos.y * 4f) / 4f, Mathf.RoundToInt(groundPos.z));
-                DemoObject.position = roundedPos;
+                GridColumn column = Grid.GetColumn(groundPos);
+                //Vector3 roundedPos = new Vector3(Mathf.RoundToInt(groundPos.x), Mathf.RoundToInt(groundPos.y * 4f) / 4f, Mathf.RoundToInt(groundPos.z));
+                DemoObject.position = column.TopPos;
             }
         }
     }
